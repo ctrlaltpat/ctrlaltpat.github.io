6e0b3217da8cd087f319f52a7017f523a7d572ab
Patrick <ctrlaltpat@gmail.com>
2020-01-13 22:14:37 +0000

    out subtree commit

---
+++ out/_next/static/webpack/static/development/pages/index.js.480cbf730d035a06d876.hot-update.js
@@ -0,0 +1,56 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/rick/Desktop/ctrlaltpat.github.io/pages/index.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var Home = function Home() {
+  return __jsx("div", {
+    className: "flex items-center flex-col justify-center h-screen bg-black",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, __jsx("div", {
+    className: "relative w-64 h-64",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, __jsx("img", {
+    className: "absolute h-full w-full object-cover rounded-full shadow-md",
+    src: "/ctrlaltpat1.png",
+    alt: "ctrlaltpat memoji",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  })), __jsx("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, "CtlAltPat"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.480cbf730d035a06d876.hot-update.js.map
