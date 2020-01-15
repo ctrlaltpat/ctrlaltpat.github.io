c7729c06366e3989e615947db6183a0a877184db
Patrick <ctrlaltpat@gmail.com>
2020-01-15 15:44:02 +0000

    add Head with title

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Head from "next/head";
 import Social from "../components/Social";
 
 const Home = () => (
   <div className="flex items-center flex-col justify-center h-screen cap-bg">
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>CtrlAltPat</title>
+    </Head>
     <div className="relative w-64 h-64">
       <img
         className="avatar"
